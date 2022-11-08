3100ef469fc45882f8d149de2b7f04cde439d39c
Rojoin <nachoarrastua@gmail.com>
2022-11-08 09:40:59 -0300

    Add gameLogic Method

---
+++ src/gameScreens/gameplayState.cpp
@@ -0,0 +1,23 @@
+#include "gameplayState.h"
+
+#include "gameLogic/gameLogic.h"
+#include "gameObjects/Character.h"
+#include "gameObjects/Obstacle.h"
+#include "gameScreens/gameStatesEnum.h"
+
+Character* character = new Character();
+Obstacle* obstacle = new Obstacle();
+void gameLogic()
+{
+	if (character->isPlayerDead() != true)
+	{
+		character->changePos();
+		obstacle->changePosX();
+
+	}
+	else
+	{
+		setGameState(GameStates::Menu);
+	}
+	character->setPlayerDeadStatus(isCharacterObstacleColliding(character, obstacle));
+}
