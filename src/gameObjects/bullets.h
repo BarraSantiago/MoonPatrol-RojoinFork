04e120cf8c0b0cbc963c2afe9c336ac1da36a872
Rojoin <nachoarrastua@gmail.com>
2022-11-15 09:25:11 -0300

    Add bullets Class

---
+++ src/gameObjects/bullets.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <raylib.h>
+#include "system/circle.h"
+#include "GameObjectsEnums.h"
+
+
+namespace GameObjects
+{
+
+	struct Bullet
+	{
+		BulletType type;
+		Texture texture;
+		Circle circle;
+		Vector2 aceleration;
+		Sound sound;
+		float rotation;
+		bool isActive;
+		float scale;
+		float speed;
+
+	};
+	const int maxBullets = 20;
+	Bullet createBullet(float x, float y);
+	void changeBulletPosition(Bullet& bullet);
+	void drawBullet(Bullet& bullet);
+	const float BULLET_MAX_SPEED = 500.0f;
+}
+
+
